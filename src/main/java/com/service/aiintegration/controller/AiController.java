95e76a1679d110ca8877eeb284b82707d704ba57
vadikamar <87433086+vadikamar@users.noreply.github.com>
2025-12-28 21:44:36 +0530

    Cors properties added

--- src/main/java/com/service/aiintegration/controller/AiController.java
+++ src/main/java/com/service/aiintegration/controller/AiController.java
@@ -8,14 +8,11 @@ import com.service.aiintegration.service.AiService;
 import com.service.aiintegration.service.RagService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/ai")
